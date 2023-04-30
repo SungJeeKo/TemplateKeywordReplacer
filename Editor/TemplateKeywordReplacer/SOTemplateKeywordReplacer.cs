3830e2b712f5baa378b1a42fb86787db1470dad0
SungJeeKo <rhtjdwl1227@naver.com>
2023-04-30 16:54:48 +0900

    [dev] Fix some troubles and add guide

--- Editor/TemplateKeywordReplacer/SOTemplateKeywordReplacer.cs
+++ Editor/TemplateKeywordReplacer/SOTemplateKeywordReplacer.cs
@@ -8,6 +8,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "SOTemplateKeyword", menuName = "[DevMenu]/SOTemplatekeyword")]
 public class SOTemplateKeywordReplacer : ScriptableObject
 {
     public string Namespace;
