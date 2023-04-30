08610c006e4a5be25f667e1ad7f36bc18d38e56d
SungJeeKo <rhtjdwl1227@naver.com>
2023-04-30 16:43:24 +0900

    [dev] Add Template Keyword Replacer

---
+++ Editor/TemplateKeywordReplacer/SOTemplateKeywordReplacer.cs
@@ -0,0 +1,15 @@
+/*
+SOTemplateKeywordReplacer.cs
+Author : Koo
+Date   : 2023-04-29 00:15:03
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SOTemplateKeywordReplacer : ScriptableObject
+{
+    public string Namespace;
+    public string Author;
+}
---
+++ Editor/TemplateKeywordReplacer/TemplateKeywordReplacer.cs
@@ -0,0 +1,47 @@
+/*
+TemplateKeywordReplacer.cs
+Author : Koo
+Date   : 2023-04-29 00:15:03
+*/
+
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class TemplateKeywordReplacer : UnityEditor.AssetModificationProcessor
+{
+    public static void OnWillCreateAsset(string path)
+    {
+        path = path.Replace(".meta", "");
+        int index = path.LastIndexOf(".");
+
+        if (index < 0) { return; }
+
+        string file = path.Substring(index);
+        if (file != ".cs") { return; }
+
+        index = Application.dataPath.LastIndexOf("Assets");
+        path = Application.dataPath.Substring(0, index) + path;
+        if (!System.IO.File.Exists(path)) { return; }
+
+        string fileContent = System.IO.File.ReadAllText(path);
+
+        fileContent = fileContent.Replace("#DATE#", System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss", System.Globalization.CultureInfo.CreateSpecificCulture("ko-KR")));
+        SOTemplateKeywordReplacer information = AssetDatabase.LoadAssetAtPath<SOTemplateKeywordReplacer>("Assets/Editor/TemplateKeyword.asset");
+        if (information)
+        {
+            fileContent = fileContent.Replace("#NAMESPACE#", information.Namespace);
+            fileContent = fileContent.Replace("#AUTHOR#", information.Author);
+        }
+        else
+        {
+            Debug.LogError("Failed to load DeveloperInformation");
+        }
+
+        // 대체가 끝나면 다시 파일에 쓰기
+        System.IO.File.WriteAllText(path, fileContent);
+
+        // 다하고 나면 꼭 호출
+        AssetDatabase.Refresh();
+    }
+}
